dc674bcff05865b1929b73ddc6b00723c6008a73
Gamenstein <39507120+Gamenstein@users.noreply.github.com>
2019-07-27 15:27:31 -0400

    Main and Pause Menu Base Setup

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Image controlPanel;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Play");
+    }
+
+    public void OpenControls()
+    {
+        controlPanel.gameObject.SetActive(true);
+    }
+
+    public void CloseControls()
+    {
+        controlPanel.gameObject.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public Image controlPanel;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 1f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale= 0f;
+    }
+
+    public void OpenControls()
+    {
+        controlPanel.gameObject.SetActive(true);
+    }
+
+    public void CloseControls()
+    {
+        controlPanel.gameObject.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+;
