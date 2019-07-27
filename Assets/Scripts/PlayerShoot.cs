e8dafd231bd8dcd739393adc38df27d77561a862
Gamenstein <39507120+Gamenstein@users.noreply.github.com>
2019-07-27 12:49:05 -0400

    Movement and Shooting Update

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject standardProjectile;
+    public Transform firingPoint, forwardFire, upFire, downFire;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void Shoot(float direction)
+    {
+        if(direction == 0)
+        {
+            firingPoint = forwardFire;
+        }
+        else if(direction == 1)
+        {
+            firingPoint = upFire;
+        }
+        else if(direction == -1)
+        {
+            firingPoint = downFire;
+        }
+
+        GameObject fireShot = Instantiate(standardProjectile, firingPoint.position, firingPoint.rotation);
+    }
+}
