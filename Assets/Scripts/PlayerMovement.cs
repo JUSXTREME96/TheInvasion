714652304f66836bb5ee58c583e904211a885770
Gamenstein <39507120+Gamenstein@users.noreply.github.com>
2019-07-27 11:11:11 -0400

    Scripts

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Verticakl"), 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+}
