1e6e0df3947a0828ab2a930af442c0879be1d3d8
Gamenstein <39507120+Gamenstein@users.noreply.github.com>
2019-07-27 11:21:49 -0400

    Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed;
+    public float currentSpeed, minimumSpeed, maximumSpeed, acceleration;
 
     void Start()
     {
@@ -14,7 +14,24 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Verticakl"), 0f);
-        transform.position += movement * Time.deltaTime * moveSpeed;
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+
+        if(Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if(currentSpeed < maximumSpeed)
+            {
+                currentSpeed += Time.deltaTime * acceleration;
+            }
+            else
+            {
+                currentSpeed = maximumSpeed;
+            }
+        }
+        else
+        {
+            currentSpeed = minimumSpeed;
+        }
+
+        transform.position += movement * Time.deltaTime * currentSpeed;
     }
 }
