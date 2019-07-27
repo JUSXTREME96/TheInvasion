c67d1f2575fe4549bff2e1408db97b4c38ded6cb
Gamenstein <39507120+Gamenstein@users.noreply.github.com>
2019-07-27 11:55:18 -0400

    Player Movement Overhaul

---
+++ Assets/Scripts/PlayerDash.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private int direction;
+
+    void Start()
+    {
+        dashTime = startDashTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction != 0)
+        {
+            if (dashTime <= 0)
+            {
+                direction = 0;
+                dashTime = startDashTime;
+                rigidBody.velocity = Vector2.zero;
+            }
+            else
+            {
+                dashTime -= Time.deltaTime;
+
+                if (direction == 1)
+                {
+                    rigidBody.velocity = Vector2.right * dashSpeed;
+                }
+                else if (direction == 2)
+                {
+                    rigidBody.velocity = Vector2.left * dashSpeed;
+                }
+            }
+        }
+    }
+
+    public void Dash(float directionPassed)
+    {
+        if (directionPassed > 0)
+        {
+            direction = 1;
+        }
+        else
+        {
+            direction = 2;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Player))]
+public class PlayerInput : MonoBehaviour {
+
+	Player player;
+    public PlayerDash playerDash;
+
+	void Start () {
+		player = GetComponent<Player> ();
+	}
+
+	void Update () {
+		Vector2 directionalInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		player.SetDirectionalInput (directionalInput);
+
+        if (Input.GetButtonDown("Fire2") && Input.GetAxisRaw("Horizontal") > 0)
+        {
+            playerDash.Dash(1);
+        }
+
+        if (Input.GetButtonDown("Fire2") && Input.GetAxisRaw("Horizontal") < 0)
+        {
+            playerDash.Dash(-1);
+        }
+
+        if (Input.GetButtonDown("Jump")) {
+			player.OnJumpInputDown ();
+		}
+		if (Input.GetButtonUp("Jump")) {
+			player.OnJumpInputUp ();
+		}
+	}
+}
